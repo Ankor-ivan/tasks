81dd6340a74002ecbc17d78f49f7147f01e7a571
ivan <Ankor-ivan@mail.ru>
2019-04-14 17:18:12 +0300

    это первый комит

---
+++ task.js
@@ -0,0 +1,20 @@
+'usestrict';
+let money = prompt('Ваш бюджет на месяц?','');
+    time = prompt('Введите дату в формате YYYY-MM-DD','');
+    let appdata = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+optionalexpenses: {},  
+ income: [],
+ savings: false
+    };
+let a1 = prompt( "Введите обязательную статью расходов в этом месяце", ''),
+    a2 = prompt( "Во сколько обойдется?", ''),
+    a3 = prompt( "Введите обязательную статью расходов в этом месяце", ''),
+    a4 = prompt( "Во сколько обойдется?", '');
+
+    appdata.expenses.a1 = a2;
+    appdata.expenses.a3 = a4;
+    
+alert(appdata.budget / 30);
